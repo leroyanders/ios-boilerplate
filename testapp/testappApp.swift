1d31f61586a6d630ab05550de8a5636851f151b5
leroyanders <leroy.wagner20@gmail.com>
2026-01-18 15:11:00 +0200

    Initial Commit

---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Leroy Anders on 18.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
